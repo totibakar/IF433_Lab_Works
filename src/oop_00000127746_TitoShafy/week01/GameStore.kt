7c54a8b3867a29bf00ab2ef7ca85863c19c59a7c
unknown <tito.shafy@student.umn.ac.id>
2026-02-02 16:45:26 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000127746_TitoShafy/week01/GameStore.kt
+++ src/oop_00000127746_TitoShafy/week01/GameStore.kt
@@ -1,12 +1,28 @@
 package oop_00000127746_TitoShafy.week01
 
 fun main() {
-    val gameTitle = "Microsoft Flight Simulator (2020) Premium Deluxe 40th"
-    val price = 900000
+    val gameTitle = "Elden Ring: Shadow of the Erdtree"
+    val price = 599000
+    val userNote: String? = null
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+
+    printReceipt(
+        title = gameTitle,
+        finalPrice = finalPrice,
+        note = userNote
+    )
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) {
     (price * 0.2).toInt()
 } else {
     (price * 0.1).toInt()
+}
+
+fun printReceipt(title: String, finalPrice: Int, note: String?) {
+    println("Struk Pembelian SteamKW")
+    println("Game: $title")
+    println("Harga Akhir: Rp $finalPrice")
+    println("Catatan: ${note ?: "Tidak ada catatan"}")
 }
