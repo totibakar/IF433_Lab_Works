12cf9b779355f49e10dd141eca43bc93e9348232
unknown <tito.shafy@student.umn.ac.id>
2026-02-02 16:34:58 +0700

    week01: (tugas 1) add legacy CircleCalculator code

--- src/oop_00000127746_TitoShafy/week01/CircleCalculator.kt
+++ src/oop_00000127746_TitoShafy/week01/CircleCalculator.kt
@@ -1,4 +1,19 @@
 package oop_00000127746_TitoShafy.week01
 
-class CircleCalculator {
+fun main(args: Array<String>) {
+    val radius = 7.0
+    val pi = 3.14
+    val area = pi * radius * radius
+
+    println("Radius: $radius, Area: $area")
+
+    checkSize(area)
+}
+
+fun checkSize(area: Double) {
+    if (area > 100) {
+        println("This is a Big Circle")
+    } else {
+        println("This is a Small Circle")
+    }
 }
