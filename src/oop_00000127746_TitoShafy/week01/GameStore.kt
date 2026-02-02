b2d07fd5903c65a64cc1b19f3a1168d2eaa9d9d8
unknown <tito.shafy@student.umn.ac.id>
2026-02-02 16:43:59 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000127746_TitoShafy/week01/GameStore.kt
+++ src/oop_00000127746_TitoShafy/week01/GameStore.kt
@@ -4,3 +4,9 @@ fun main() {
     val gameTitle = "Microsoft Flight Simulator (2020) Premium Deluxe 40th"
     val price = 900000
 }
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) {
+    (price * 0.2).toInt()
+} else {
+    (price * 0.1).toInt()
+}
