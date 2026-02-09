fe8b89b969c97dfbacf61502c1da13382ad4ec2e
totibakar <tito.shafy@student.umn.ac.id>
2026-02-09 17:44:15 +0900

    week02: implement selection logic for constructors

--- src/oop_00000127746_TitoShafy/week02/Main.kt
+++ src/oop_00000127746_TitoShafy/week02/Main.kt
@@ -14,10 +14,21 @@ fun main() {
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
     } else {
-        print("Masukkan Jurusan")
-        val major = scanner.nextLine()
+        print("Pilih jalur (1. Reguler, 2. Umum: ")
+        val type = scanner.nextInt()
+        scanner.nextLine()
 
-        val s1= Student(name, nim, major)
-        print("Status: Pendaftaran Selesai.")
+        if (type == 1) {
+            print("Masukkan Jurusan: ")
+            val major = scanner.nextLine()
+            val s1 = Student(name, nim, major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+            println("Status: Pendaftaran selesai.")
+        } else if (type == 2) {
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
     }
 }
--- src/oop_00000127746_TitoShafy/week02/Student.kt
+++ src/oop_00000127746_TitoShafy/week02/Student.kt
@@ -1,6 +1,6 @@
 package oop_00000127746_TitoShafy.week02
 
-class Student (val name: String, val nim: String, var gpa: Double = 0.0, var major: String) {
+class Student (val name: String, val nim: String, var major: String, var gpa: Double = 0.0) {
     init {
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
