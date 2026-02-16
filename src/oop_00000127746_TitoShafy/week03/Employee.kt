cb0d951ffb3252d2b87392b1cbe1255b6609a4ab
totibakar <tito.shafy@student.umn.ac.id>
2026-02-16 15:26:20 +0900

    week03: test all encapsulation logic in main

--- src/oop_00000127746_TitoShafy/week03/Employee.kt
+++ src/oop_00000127746_TitoShafy/week03/Employee.kt
@@ -20,4 +20,7 @@ class Employee(val name: String) {
     fun printStatus() {
         println("Karyawan: $name, Rating: $perfomanceRating")
     }
+
+    val tax: Double
+        get() = salary * 0.1
 }
--- src/oop_00000127746_TitoShafy/week03/Main.kt
+++ src/oop_00000127746_TitoShafy/week03/Main.kt
@@ -2,6 +2,12 @@ package oop_00000127746_TitoShafy.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
+
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    e.increasePerfomance()
+
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
