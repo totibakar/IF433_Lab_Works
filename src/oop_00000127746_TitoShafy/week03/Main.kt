bfec9c178d408d9a127715370b780153c071ba59
totibakar <tito.shafy@student.umn.ac.id>
2026-02-16 15:29:05 +0900

    week03: cleanup code

--- src/oop_00000127746_TitoShafy/week03/Main.kt
+++ src/oop_00000127746_TitoShafy/week03/Main.kt
@@ -3,15 +3,11 @@ package oop_00000127746_TitoShafy.week03
 fun main() {
     val e = Employee("Budi")
 
-    // 1. test validasi salary
-    e.salary = -1000 // harusnya print error
+    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    // 2. test encapsulation
     e.increasePerfomance()
-    // e.perfomanceRating = 5
 
-    // 3. test computed property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
