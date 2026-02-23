ec394835ef23c54b71689058bc89e2fa10e967ad
totibakar <tito.shafy@student.umn.ac.id>
2026-02-23 14:32:38 +0900

    week04: (task 1) test ElectricCar in main

--- src/oop_00000127746_TitoShafy/week04/Main.kt
+++ src/oop_00000127746_TitoShafy/week04/Main.kt
@@ -11,4 +11,10 @@ fun main(){
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+
+    println("\n--- Testing Electric Car ---")
+    val myElectricCar = ElectricCar("Porsche", 4, 100)
+    myElectricCar.openTrunk()
+    myElectricCar.honk()
+    myElectricCar.accelerate()
 }
