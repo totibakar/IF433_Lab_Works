e768055c893b0bf957a9d1aa1fb38243dfe1f9ed
totibakar <tito.shafy@student.umn.ac.id>
2026-02-23 14:22:18 +0900

    week04: test Vehicle and Car in main

---
+++ src/oop_00000127746_TitoShafy/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000127746_TitoShafy.week04
+
+fun main(){
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car("Toyota", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
