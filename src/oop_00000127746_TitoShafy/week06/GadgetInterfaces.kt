11bf068ddb9cfe0e4eb5d06f36536b164d0e8ac2
totibakar <tito.shafy@student.umn.ac.id>
2026-03-09 12:43:49 +0900

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000127746_TitoShafy/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000127746_TitoShafy.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
