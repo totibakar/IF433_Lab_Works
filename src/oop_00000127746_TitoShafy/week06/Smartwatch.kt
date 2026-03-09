0ae249090950312504684791cfa6289d86fbbcbc
totibakar <tito.shafy@student.umn.ac.id>
2026-03-09 12:41:47 +0900

    week06: create Smartwatch with multiple implementations

---
+++ src/oop_00000127746_TitoShafy/week06/Smartwatch.kt
@@ -0,0 +1,17 @@
+package oop_00000127746_TitoShafy.week06
+
+class Smartwatch : Watch(), BluetoothConnectable, Rechargeable {
+    override fun showTime() {
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+
+    override fun connectToBluetooth() {
+        println("Mencari perangkat HP di sekitar untuk pairing...")
+    }
+
+    override fun chargeBattery() {
+        println("Mengisi daya menggunakan charger magnetik 15W.")
+    }
+}
+
+
