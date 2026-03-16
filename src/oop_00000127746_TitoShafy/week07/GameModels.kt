49d9f5679bb78e8a09939ceb370719bd3bc130a1
totibakar <tito.shafy@student.umn.ac.id>
2026-03-17 08:00:51 +0900

    week07: (task) create GameItem data class

--- src/oop_00000127746_TitoShafy/week07/GameModels.kt
+++ src/oop_00000127746_TitoShafy/week07/GameModels.kt
@@ -3,3 +3,5 @@ package oop_00000127746_TitoShafy.week07
 enum class ItemRarity(val dropChance: Int) {
     COMMON(70), UNCOMMON(40), RARE(20), EPIC(5), LEGENDARY(1)
 }
+
+data class GameItem(val name: String, val damage: Int, val rarity: ItemRarity)
