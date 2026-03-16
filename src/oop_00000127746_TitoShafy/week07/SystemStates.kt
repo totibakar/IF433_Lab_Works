cfd0f97835774222dbdf83971619cc75fe1b79c1
totibakar <tito.shafy@student.umn.ac.id>
2026-03-17 07:55:50 +0900

    week07: create ApiResponse sealed class

--- src/oop_00000127746_TitoShafy/week07/SystemStates.kt
+++ src/oop_00000127746_TitoShafy/week07/SystemStates.kt
@@ -3,3 +3,9 @@ package oop_00000127746_TitoShafy.week07
 enum class AppState {
     STARTING, RUNNING, STOPPED
 }
+
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
+}
