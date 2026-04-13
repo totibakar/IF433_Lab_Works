259d60f2a0ec18f46921d5c02333e54c5301e494
totibakar <tito.shafy@student.umn.ac.id>
2026-04-13 14:49:58 +0900

    week08: (trial) trigger NullPointerExecption with !!

--- src/oop_00000127746_TitoShafy/week08/Main.kt
+++ src/oop_00000127746_TitoShafy/week08/Main.kt
@@ -37,4 +37,11 @@ fun main() {
     val safeString = someObject as? String ?: "Unknown String"
     println("Hasil cast + fallback: $safeString")
 
+    println("\n=== TEST THE RED BUTTON (!!) ===")
+    val toxicData: String? = null
+    try {
+        val lenght = toxicData!!.length
+    } catch (e: NullPointerException) {
+        println("CRASH (NPE)! Jangan gunakan !! secara sembarangan")
+    }
 }
