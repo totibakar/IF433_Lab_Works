9c7418f5a74e238d8be27cd5f3254c27611b2a3d
totibakar <tito.shafy@student.umn.ac.id>
2026-04-13 14:20:45 +0900

    week08: create nested classes for Order

---
+++ src/oop_00000127746_TitoShafy/week08/OrderModels.kt
@@ -0,0 +1,6 @@
+package oop_00000127746_TitoShafy.week08
+
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
