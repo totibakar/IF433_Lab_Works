b14c96f5a6197f7f65a56cf810c927e611dc3770
totibakar <tito.shafy@student.umn.ac.id>
2026-04-13 15:06:16 +0900

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_00000127746_TitoShafy/week08/EcommerceModels.kt
@@ -0,0 +1,15 @@
+package oop_00000127746_TitoShafy.week08
+
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
