5d1635a14d5af1933ca68eb5dd74f7609c181cf3
totibakar <tito.shafy@student.umn.ac.id>
2026-04-13 14:27:46 +0900

    week08: implement safe call with let block

--- src/oop_00000127746_TitoShafy/week08/Main.kt
+++ src/oop_00000127746_TitoShafy/week08/Main.kt
@@ -6,4 +6,13 @@ fun main() {
 
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("Tujuan pengiriman: $destination")
+
+    println("\n=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 250000)
+    val receipt = validOrder.totalPrice?.let { price ->
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+
+    println(receipt)
 }
