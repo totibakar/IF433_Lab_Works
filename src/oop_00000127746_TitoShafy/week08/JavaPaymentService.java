cdd8af5e33de68ef87332539b163b18417f8a17f
totibakar <tito.shafy@student.umn.ac.id>
2026-04-13 15:13:29 +0900

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000127746_TitoShafy/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000127746_TitoShafy.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
