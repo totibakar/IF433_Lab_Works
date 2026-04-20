aaf653ab1816f3492c3f5ad8e36396277aee1de5
totibakar <tito.shafy@student.umn.ac.id>
2026-04-20 15:30:59 +0900

    week09: (task) create TradeLog data class

---
+++ src/oop_00000127746_TitoShafy/week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_00000127746_TitoShafy.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
