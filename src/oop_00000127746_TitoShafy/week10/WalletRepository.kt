c6e67f731e4d12ed0eb893ae5a2aeda41193dc82
totibakar <tito.shafy@student.umn.ac.id>
2026-04-27 13:52:34 +0900

    week10: (task) implement search logic in repository

--- src/oop_00000127746_TitoShafy/week10/WalletRepository.kt
+++ src/oop_00000127746_TitoShafy/week10/WalletRepository.kt
@@ -3,7 +3,15 @@ package oop_00000127746_TitoShafy.week10
 class WalletRepository<T> {
     private val items = mutableListOf<T>()
 
-    fun add(item: T) {}
+    fun add(item: T) {
+        items.add(item)
+    }
 
-    fun getAll(): List<T> {}
+    fun getAll(): List<T> {
+        return items
+    }
+
+    fun search(query: (T) -> Boolean, list: List<T>): List<T> {
+        return items.filter(query)
+    }
 }
