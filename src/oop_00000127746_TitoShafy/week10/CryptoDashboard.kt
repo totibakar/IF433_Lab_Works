cf241b3ce12ddb2258df183558b1a9b622ad7e1c
totibakar <tito.shafy@student.umn.ac.id>
2026-04-27 13:54:43 +0900

    week10: (task) populate coin repository

--- src/oop_00000127746_TitoShafy/week10/CryptoDashboard.kt
+++ src/oop_00000127746_TitoShafy/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000127746_TitoShafy.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.523))
+    coinRepo.add(Coin("ETH", 4.12))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
