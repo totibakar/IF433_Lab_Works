4cfd9cd39e4f70ee48919f72fed9751a41975dff
totibakar <tito.shafy@student.umn.ac.id>
2026-04-27 13:55:33 +0900

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000127746_TitoShafy/week10/CryptoDashboard.kt
+++ src/oop_00000127746_TitoShafy/week10/CryptoDashboard.kt
@@ -6,4 +6,6 @@ fun main() {
     coinRepo.add(Coin("BTC", 0.523))
     coinRepo.add(Coin("ETH", 4.12))
     coinRepo.add(Coin("USDT", 1500.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
