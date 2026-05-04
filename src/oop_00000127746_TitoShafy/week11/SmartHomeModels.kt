c49ca6ec3ba42ac25d17e4d02ef9531ce35e35e6
totibakar <tito.shafy@student.umn.ac.id>
2026-05-04 14:56:24 +0900

    week11: (task) create extension function for SmartDevice

--- src/oop_00000127746_TitoShafy/week11/SmartHomeModels.kt
+++ src/oop_00000127746_TitoShafy/week11/SmartHomeModels.kt
@@ -1,3 +1,6 @@
 package oop_00000127746_TitoShafy.week11
 
 data class SmartDevice(var name: String, var category: String, var isOnline: Boolean = false, var powerLoad: Int = 0)
+fun SmartDevice.diagnose(): String{
+    return "[DIAGNOSTIKE] $name | Kategori: $category | Status: ${if (isOnline) "Online" else "Offline"} | Daya: $powerLoad Watt"
+}
