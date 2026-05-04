cb3ad9b60f3802b0b666090c9e1c3d59b4b402b2
totibakar <tito.shafy@student.umn.ac.id>
2026-05-04 15:01:50 +0900

    week11: (task) implement device search with let

--- src/oop_00000127746_TitoShafy/week11/SmartHomeDashboard.kt
+++ src/oop_00000127746_TitoShafy/week11/SmartHomeDashboard.kt
@@ -22,4 +22,10 @@ fun main() {
     }
     homeDevices.add(acUnit)
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println("\n=== HASIL PENCARIAN ===")
+        println(it.diagnose())
+    }
 }
