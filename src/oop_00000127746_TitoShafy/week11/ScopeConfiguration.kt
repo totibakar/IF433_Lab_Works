46819882468cc19e5e18052b06313c696b25b7e7
totibakar <tito.shafy@student.umn.ac.id>
2026-05-04 14:40:38 +0900

    week11: implement apply for object initialization

--- src/oop_00000127746_TitoShafy/week11/ScopeConfiguration.kt
+++ src/oop_00000127746_TitoShafy/week11/ScopeConfiguration.kt
@@ -1,3 +1,12 @@
 package oop_00000127746_TitoShafy.week11
 
 data class User(var name: String = "", var age: Int = 0)
+
+fun main() {
+    println("=== TEST APPLY ===")
+    val user = User().apply {
+        name = "Alex"
+        age = 25
+    }
+    println(user)
+}
