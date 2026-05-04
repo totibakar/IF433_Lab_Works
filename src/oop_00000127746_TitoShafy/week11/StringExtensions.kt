30a2ee044bfc67c7fd25fd2e7f13d451906f8d81
totibakar <tito.shafy@student.umn.ac.id>
2026-05-04 14:26:53 +0900

    week11: create string extension functions

---
+++ src/oop_00000127746_TitoShafy/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_00000127746_TitoShafy.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
