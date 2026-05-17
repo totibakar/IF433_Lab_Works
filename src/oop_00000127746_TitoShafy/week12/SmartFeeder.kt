65b843fec32787ffe5e8f9ad5e8085ef75f117ab
totibakar <tito.shafy@student.umn.ac.id>
2026-05-17 22:52:46 +0900

    week12: (task) implement hardware validation with throw

--- src/oop_00000127746_TitoShafy/week12/SmartFeeder.kt
+++ src/oop_00000127746_TitoShafy/week12/SmartFeeder.kt
@@ -2,4 +2,8 @@ package oop_00000127746_TitoShafy.week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean) : Int {
     require(requestedGram > 0) {"Porsi kibble harus lebih dari 0 gr"}
+
+    if(isJammed) {
+        throw DispenserJamException()
+    }
 }
