ae81939f14f4aa6ef63a509f325348d95148b5fc
totibakar <tito.shafy@student.umn.ac.id>
2026-05-18 15:03:39 +0900

    week13: (task) implement saveTrades using use block and printWriter

--- src/oop_00000127746_TitoShafy/week13/CryptoTradeLogger.kt
+++ src/oop_00000127746_TitoShafy/week13/CryptoTradeLogger.kt
@@ -20,3 +20,9 @@ fun fromCsvTrade(line: String): TradeRecord? {
         null
     }
 }
+
+fun saveTrades(trades: List<TradeRecord>, path: String) {
+    File(path).printWriter().use { writer ->
+        trades.forEach { writer.println(it.toCsv()) }
+    }
+}
