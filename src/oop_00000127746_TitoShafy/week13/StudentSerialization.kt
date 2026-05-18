2513639a1e94e36a36d26ba6be847e59fba22355
totibakar <tito.shafy@student.umn.ac.id>
2026-05-18 14:44:57 +0900

    week13: create save and load list functions

--- src/oop_00000127746_TitoShafy/week13/StudentSerialization.kt
+++ src/oop_00000127746_TitoShafy/week13/StudentSerialization.kt
@@ -9,3 +9,16 @@ fun fromCsv(line: String): Student {
     val parts = line.split(",")
     return Student(parts[0], parts[1].toInt(), parts[2].toDouble())
 }
+
+fun saveStudents(students: List<Student>, path: String) {
+    File(path).writeText(students.joinToString("\n") { it.toCsv() })
+}
+
+fun loadStudents(path: String): List<Student> {
+    return try {
+        File(path).readLines().map { fromCsv(it) }
+    } catch (e: FileNotFoundException) {
+        println("Error: File tidak ditemukan!")
+        emptyList()
+    }
+}
