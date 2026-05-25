4f47783f990a3f9c2b0d1ec6b8bc49ac09ff8763
totibakar <tito.shafy@student.umn.ac.id>
2026-05-25 13:52:14 +0900

    week14: implement basic writeText function

--- src/oop_00000127746_TitoShafy/week14/SrpViolation.kt
+++ src/oop_00000127746_TitoShafy/week14/SrpViolation.kt
@@ -1,2 +1,18 @@
 package oop_00000127746_TitoShafy.week14
 
+data class User(val name: String, val email: String, val age: Int)
+
+class UserManager {
+
+    fun validateUser(user: User): Boolean {
+        return user.email.contains("@") && user.age >= 18
+    }
+
+    fun saveUserToDatabase(user: User) {
+        println("INSERT INTO users VALUES ('${user.name}', '${user.email}')")
+    }
+
+    fun sendWelcomeEmail(user: User) {
+        println("Sending email to: ${user.email}")
+    }
+}
